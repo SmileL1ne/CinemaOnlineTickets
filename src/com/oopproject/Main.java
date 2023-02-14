6e6d88845cf3308db3bbb2ecd71fe4a6aa8ff9f1
Altynbek <rysbek20051@gmail.com>
2023-02-15 04:24:09 +0600

    user register

--- src/com/oopproject/Main.java
+++ src/com/oopproject/Main.java
@@ -8,6 +8,7 @@ import com.oopproject.Users.UserService;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -31,8 +32,53 @@ public class Main {
             System.out.println("Invalid input. Try again");
             return;
         }
+    }
+    public class User {
+        private String username;
+        private String password;
 
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
 
+        public String getUsername() {
+            return username;
+        }
 
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    public class UserRegistry {
+        private ArrayList<User> users;
+
+        public UserRegistry() {
+            this.users = new ArrayList<>();
+        }
+
+        public void registerUser(String username, String password) {
+            User user = new User(username, password);
+            users.add(user);
+        }
+
+        public boolean checkUsernameExists(String username) {
+            for (User user : users) {
+                if (user.getUsername().equals(username)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public boolean checkPasswordMatch(String username, String password) {
+            for (User user : users) {
+                if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
